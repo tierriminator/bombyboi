5f834dd8388ef0ae5c077a3d8dd67cc0c610d132
Tierry Hörmann <tierry@deepjudge.ai>
2025-12-20 17:14:59 +0100

    some refactoring

--- bombyboi/player.gd
+++ bombyboi/player.gd
@@ -18,23 +18,27 @@ func check_terrain(p_movedir: Vector2i) -> TileData:
 	return terrainmap_path.get_cell_tile_data(p_movedir)
 
 func _physics_process(delta: float) -> void:
-	var movedir: Vector2i
-	if Input.is_action_just_pressed(movedown):
-		movedir = Vector2i(0,1)
-	elif Input.is_action_just_pressed(moveup):
-		movedir = Vector2i(0,-1)
-	elif Input.is_action_just_pressed(moveright):
-		movedir = Vector2i(1,0)
-	elif Input.is_action_just_pressed(moveleft):
-		movedir = Vector2i(-1,0)
-	else:
-		movedir = Vector2i(0,0)
 	
 	map_position = terrainmap_path.local_to_map(position)
+	move(map_position)
+	maybe_place_bomb(map_position)
+		
+func move(map_position: Vector2i) -> void:
+	var movedir: Vector2i = Vector2i(0, 0)
+	if Input.is_action_just_pressed(movedown):
+		movedir += Vector2i(0,1)
+	if Input.is_action_just_pressed(moveup):
+		movedir += Vector2i(0,-1)
+	if Input.is_action_just_pressed(moveright):
+		movedir += Vector2i(1,0)
+	if Input.is_action_just_pressed(moveleft):
+		movedir += Vector2i(-1,0)
+	
 	movedir = map_position + movedir
 	if check_terrain(movedir).get_collision_polygons_count(0) == 0:
 		set_position(terrainmap_path.map_to_local(movedir))
-
+		
+func maybe_place_bomb(map_position: Vector2i) -> void:
 	if Input.is_action_just_pressed(place_bomb):
 		var bomb = bomb_scene.instantiate()
 		bomb.position = terrainmap_path.map_to_local(map_position)

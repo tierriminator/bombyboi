a00c99f185bbf041573684e8134158b0c3ad5fb4
Tierry Hörmann <tierry@deepjudge.ai>
2025-12-20 22:12:49 +0100

    add spawn_energy and spawn_bomb to map

---
+++ bombyboi/energy.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var type: Main.EnergyType
+
+var green_texture = preload("res://Art/energy.png")
+var pink_texture = preload("res://Art/energy2.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	match type:
+		Main.EnergyType.GREEN:
+			$Sprite2D.texture = green_texture
+		Main.EnergyType.PINK:
+			$Sprite2D.texture = pink_texture
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

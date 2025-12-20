17b7f2aae48b14d72e3ed296246763c3559f02f9
Mirafnir <linusdotarnold@gmail.com>
2025-12-20 17:03:15 +0100

    multiplayer

--- bombyboi/map.gd
+++ bombyboi/map.gd
@@ -1,19 +1,26 @@
 extends Node
 
-@onready var playerCount: int
-
 var spawnpoints = {
-	"p1": "2,2",
-	"p2": "19,9"
+	1: Vector2i(1,1),
+	2: Vector2i(18,8)
 }
+
+var player_count = spawnpoints.size()
+
+@onready var terrain := get_node("/root/Map/Terrain")
+
+@export var player_scene: PackedScene
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for player in spawnpoints:
+		var psc = player_scene.instantiate()
+		psc.set_position(terrain.map_to_local(spawnpoints[player]))
+		psc.player_id = player
+		add_child(psc)
 	pass # Replace with function body.
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- bombyboi/player.gd
+++ bombyboi/player.gd
@@ -2,13 +2,12 @@ extends CharacterBody2D
 
 var map_position: Vector2i
 
-@onready var playerId: int
-@onready var moveup: String
-@onready var movedown: String
-@onready var moveright: String
-@onready var moveleft: String
+var player_id: int
 
-@onready var playermap_path := get_node("/root/Map/Players")
+@onready var moveup = "p%d_move_up" % player_id
+@onready var movedown = "p%d_move_down" % player_id
+@onready var moveright = "p%d_move_right" % player_id
+@onready var moveleft = "p%d_move_left" % player_id
 
 @onready var terrainmap_path := get_node("/root/Map/Terrain")
 
@@ -17,19 +16,19 @@ func check_terrain(p_movedir: Vector2i) -> TileData:
 
 func _physics_process(delta: float) -> void:
 	var movedir: Vector2i
-	if Input.is_action_just_pressed("p1_move_down"):
+	if Input.is_action_just_pressed(movedown):
 		movedir = Vector2i(0,1)
-	elif Input.is_action_just_pressed("p1_move_up"):
+	elif Input.is_action_just_pressed(moveup):
 		movedir = Vector2i(0,-1)
-	elif Input.is_action_just_pressed("p1_move_right"):
+	elif Input.is_action_just_pressed(moveright):
 		movedir = Vector2i(1,0)
-	elif Input.is_action_just_pressed("p1_move_left"):
+	elif Input.is_action_just_pressed(moveleft):
 		movedir = Vector2i(-1,0)
 	else:
 		movedir = Vector2i(0,0)
 	
-	map_position = playermap_path.local_to_map(position)
+	map_position = terrainmap_path.local_to_map(position)
 	movedir = map_position + movedir
 	if check_terrain(movedir).get_collision_polygons_count(0) == 0:
-		set_position(playermap_path.map_to_local(movedir))
-#	move_and_slide()
+		set_position(terrainmap_path.map_to_local(movedir))
+	#move_and_slide()

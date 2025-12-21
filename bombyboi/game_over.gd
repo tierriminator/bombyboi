3324290f7252bea87d2c1daf495a641878e536e2
Semira <peacepeace@gmx.ch>
2025-12-21 01:18:21 +0100

    es isch so cool

--- bombyboi/game_over.gd
+++ bombyboi/game_over.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AudioStreamPlayer.play()
 	for player in Main.result:
 		get_node("place_%d" %player).texture = Main.result[player] 
 
